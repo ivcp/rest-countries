d7d80da1ab8de3bb83225bec2c1529fdaa3bd7b7
ivcp <blizujemars@gmail.com>
2022-10-18 14:18:16 +0200

    added label for filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -21,23 +21,28 @@ const Filter = ({
     setFilterResults(countries.filter(country => country.region === filter));
   };
   return (
-    <select
-      className={styles.filter}
-      ref={filterRef}
-      name="filter"
-      id="filter"
-      onChange={filterCountries}
-      defaultValue=""
-    >
-      <option value="" disabled hidden>
-        Filter by Region
-      </option>
-      <option value="Africa">Africa</option>
-      <option value="Americas">America</option>
-      <option value="Asia">Asia</option>
-      <option value="Europe">Europe</option>
-      <option value="Oceania">Oceania</option>
-    </select>
+    <>
+      <label className={styles.srOnly} htmlFor="filter">
+        Filter
+      </label>
+      <select
+        className={styles.filter}
+        ref={filterRef}
+        name="filter"
+        id="filter"
+        onChange={filterCountries}
+        defaultValue=""
+      >
+        <option value="" disabled hidden>
+          Filter by Region
+        </option>
+        <option value="Africa">Africa</option>
+        <option value="Americas">America</option>
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="Oceania">Oceania</option>
+      </select>
+    </>
   );
 };
 
